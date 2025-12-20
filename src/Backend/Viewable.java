d8a5fcca78c97ed8516690fc5a5843a8522aea9a
rodina-ahmedd <ra745126@gmail,com>
2025-12-20 17:33:36 +0200

    Viewable

--- src/Backend/Viewable.java
+++ src/Backend/Viewable.java
@@ -1,7 +1,18 @@
 package Backend;
 
-import javax.xml.catalog.Catalog;
+import Exceptions.InvalidGame;
+import Exceptions.NotFoundException;
+import Exceptions.SolutionInvalidException;
+import java.io.IOException;
+
 
 public interface Viewable {
     Catalog getCatalog();
+     Game getGame(DifficultyEnum level) throws NotFoundException;
+     void driveGames(Game sourceGame) throws SolutionInvalidException;
+     String verifyGame(Game game);
+     int[] solveGame(Game game) throws InvalidGame;
+     void logUserAction(String userAction) throws IOException;
+
+
 }
