687f74ec7d76f6debe0dbd5c246f29a03868e742
Ghadeer Mohamed <es.ghadir.mohamed2023@alexu.edu.eg>
2025-12-22 02:00:53 +0200

    commitSolverEdit7

--- src/Backend/SolverObserver.java
+++ src/Backend/SolverObserver.java
@@ -1,6 +1,5 @@
 package Backend;
 
 public interface SolverObserver {
-    void onSolutionFound(int[][] solution);//<<I>> for notifying GameSolver when a valid solution is found by SolverWorker//Notify()
-
+    void onSolutionFound(int[][] solution); //<<I>> for notifying GameSolver when a valid solution is found by SolverWorker//Notify()
 }
